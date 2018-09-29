c498f600b09612d674b203d2a613a39b87dcf79e
Raichiel <Raichiel@gmail.com>
2018-09-30 00:35:49 +0300

    app create+login

---
+++ app/view/login/LoginController.js
@@ -0,0 +1,36 @@
+Ext.define('ITProSenchaTest.view.login.LoginController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.login',
+
+    onLoginClick: function (btn) {
+
+        var loginData = btn.up('form').getValues();
+
+        //Здесь вместо If должен быть нормальный аякс запрос
+        if (this.checkLogin(loginData.username) && this.checkPassword(loginData.password)) {
+
+            localStorage.setItem("TutorialLoggedIn", true);
+
+            this.getView().destroy();
+
+            Ext.create({
+                xtype: 'app-main'
+            });
+
+        } else {
+
+            var errorCmp = btn.previousSibling('[name=loginErrorState]');
+            errorCmp.update(errorCmp.invalidText);
+        }
+
+
+    },
+
+    checkLogin: function (userName) {
+        return userName === "admin"
+    },
+
+    checkPassword: function (password) {
+        return password === "padmin"
+    }
+});
